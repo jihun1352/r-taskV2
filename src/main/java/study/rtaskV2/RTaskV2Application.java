117f51fe68db84c7cac8a453cf275bc7ab9edc3c
jihun1352 <jihun5008@naver.com>
2021-07-16 12:01:24 +0900

    Initial commit

---
+++ src/main/java/study/rtaskV2/RTaskV2Application.java
@@ -0,0 +1,13 @@
+package study.rtaskV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RTaskV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RTaskV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/rtaskV2/RTaskV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.rtaskV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RTaskV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
