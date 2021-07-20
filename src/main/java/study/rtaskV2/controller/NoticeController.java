e46cd54f91f21519bb947de3b1a53355f6e1ce51
jihun1352 <jihun5008@naver.com>
2021-07-20 12:59:14 +0900

    jpa

--- src/main/java/study/rtaskV2/controller/NoticeController.java
+++ src/main/java/study/rtaskV2/controller/NoticeController.java
@@ -5,21 +5,26 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import study.rtaskV2.dto.NoticeRequestDto;
 import study.rtaskV2.dto.NoticeResponseDto;
 import study.rtaskV2.service.NoticeService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class NoticeController {
     private final NoticeService noticeService;
 
+    @PostMapping("/notice")
+    public NoticeResponseDto save(@RequestBody @Valid NoticeRequestDto noticeRequestDto) {
+        return noticeService.save(noticeRequestDto);
+    }
+
     @GetMapping("/notices")
     public Page<NoticeResponseDto> findAll(@PageableDefault(page = 0, size = 3, sort = "createdDate",
-    direction = Sort.Direction.DESC) Pageable pageable) {
-
+            direction = Sort.Direction.DESC) Pageable pageable) {
         return noticeService.findAll(pageable);
     }
 }
--- src/main/java/study/rtaskV2/dto/NoticeRequestDto.java
+++ src/main/java/study/rtaskV2/dto/NoticeRequestDto.java
@@ -7,10 +7,14 @@ import lombok.NoArgsConstructor;
 import study.rtaskV2.domain.Member;
 import study.rtaskV2.domain.Notice;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeRequestDto {
+
     private Member member;
+    @NotEmpty(message = "제목을 입력하세요.")
     private String title;
     private String content;
 
